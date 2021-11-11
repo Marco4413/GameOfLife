1cbdc09eefdd79c35cc817e3226e013d84abcc5d
Marco <46133335+Marco4413@users.noreply.github.com>
2021-11-12 00:54:49 +0100

    Added grid wrap, settings and auto-simulation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import { wCanvas, Color, UMath } from "./wCanvas/wcanvas.js";
 
 const BACKGROUND_COLOR = new Color("#000");
+const GRID_COLOR = new Color("#555");
 
 const DEAD_CELL = 0;
 const LIVE_CELL = 1;
@@ -11,6 +12,8 @@ CELL_COLORS[LIVE_CELL] = new Color("#0f0");
 
 let GRID_ORIGIN = new UMath.Vec2();
 let CELL_SIZE = 0;
+
+let wrapGrid = true;
 
 /** @type {Number} */
 let gridWidth = 5;
@@ -34,9 +37,15 @@ const resetGrid = () => {
  * @returns {Number}
  */
 const getCellIndex = (x, y) => {
-    if (x < 0 || x >= gridWidth || y < 0 || y >= gridHeight)
-        return -1;
-    return x + y * gridWidth;
+    if (wrapGrid) {
+        const wrappedX = x % gridWidth;
+        const wrappedY = y % gridHeight;
+        return (wrappedX >= 0 ? wrappedX : ( gridWidth + wrappedX )) + (wrappedY >= 0 ? wrappedY : ( gridHeight + wrappedY )) * gridWidth;
+    } else {
+        if (x < 0 || x >= gridWidth || y < 0 || y >= gridHeight)
+            return -1;
+        return x + y * gridWidth;
+    }
 }
 
 /**
@@ -111,6 +120,33 @@ const stepSimulation = () => {
     grid = newGrid;
 }
 
+let timeSinceLastStep = 0;
+let autoStepInterval = 0;
+let autoStep = false;
+
+/**
+ * @param {wCanvas} canvas
+ * @param {Number} deltaTime
+ */
+const drawGrid = (canvas, deltaTime) => {
+    canvas.stroke(GRID_COLOR);
+    for (let y = 0; y < gridHeight + 1; y++) {
+        const cY = GRID_ORIGIN.y + y * CELL_SIZE;
+        canvas.line(
+            GRID_ORIGIN.x, cY,
+            GRID_ORIGIN.x + gridWidth * CELL_SIZE, cY
+        );
+    }
+
+    for (let x = 0; x < gridWidth + 1; x++) {
+        const cX = GRID_ORIGIN.x + x * CELL_SIZE;
+        canvas.line(
+            cX, GRID_ORIGIN.y,
+            cX, GRID_ORIGIN.y + gridHeight * CELL_SIZE
+        );
+    }
+}
+
 /**
  * @param {wCanvas} canvas
  * @param {Number} deltaTime
@@ -133,31 +169,104 @@ const draw = (canvas, deltaTime) => {
             }
         }
     }
+
+    drawGrid(canvas, deltaTime);
+
+    if (autoStep) {
+        timeSinceLastStep += deltaTime;
+        if (timeSinceLastStep >= autoStepInterval) {
+            stepSimulation();
+            timeSinceLastStep = 0;
+        }
+    }
 }
 
 window.addEventListener("load", () => {
+    const recalcGridSize = (canvas) => {
+        const canvasW = canvas.element.width;
+        const canvasH = canvas.element.height;
+        CELL_SIZE = Math.min(canvasW / gridWidth, canvasH / gridHeight);
+
+        GRID_ORIGIN = new UMath.Vec2(
+            (canvasW - gridWidth  * CELL_SIZE) / 2,
+            (canvasH - gridHeight * CELL_SIZE) / 2
+        );
+    }
+
+
+    resetGrid();
     const canvas = new wCanvas({
         "onDraw": draw,
-        "onSetup": (canvas) => {
-            resetGrid();
-            setCell(2, 1, LIVE_CELL);
-            setCell(2, 2, LIVE_CELL);
-            setCell(2, 3, LIVE_CELL);
-            canvas.startLoop();
-        },
         "onResize": (canvas, canvasW, canvasH) => {
             canvas.element.width  = canvasW;
             canvas.element.height = canvasH;
-            CELL_SIZE = Math.min(canvasW / gridWidth, canvasH / gridHeight);
-
-            GRID_ORIGIN = new UMath.Vec2(
-                (canvasW - gridWidth  * CELL_SIZE) / 2,
-                (canvasH - gridHeight * CELL_SIZE) / 2
-            );
+            recalcGridSize(canvas);
         }
     });
 
-    canvas.element.addEventListener("click", () => {
-        stepSimulation();
+    /** @type {HTMLInputElement} */
+    const elAutoStep = document.getElementById("autoStep");
+    autoStep = elAutoStep.checked;
+    elAutoStep.addEventListener("change", () => {
+        autoStep = elAutoStep.checked;
+    });
+
+    /** @type {HTMLInputElement} */
+    const elAutoStepInterval = document.getElementById("autoStepInterval");
+    autoStepInterval = elAutoStepInterval.valueAsNumber;
+    elAutoStepInterval.addEventListener("change", () => {
+        if (!Number.isNaN(elAutoStepInterval.valueAsNumber)) {
+            autoStepInterval = elAutoStepInterval.valueAsNumber;
+        }
+    });
+
+    /** @type {HTMLInputElement} */
+    const elGridWidth = document.getElementById("gridWidth");
+    gridWidth = elGridWidth.valueAsNumber;
+    elGridWidth.addEventListener("change", () => {
+        if (!Number.isNaN(elGridWidth.valueAsNumber)) {
+            gridWidth = Math.floor(elGridWidth.valueAsNumber);
+            resetGrid();
+            recalcGridSize(canvas);
+        }
+    });
+
+    /** @type {HTMLInputElement} */
+    const elGridHeight = document.getElementById("gridHeight");
+    gridHeight = elGridHeight.valueAsNumber;
+    elGridHeight.addEventListener("change", () => {
+        if (!Number.isNaN(elGridHeight.valueAsNumber)) {
+            gridHeight = Math.floor(elGridHeight.valueAsNumber);
+            resetGrid();
+            recalcGridSize(canvas);
+        }
+    });
+
+    document.getElementById("resetGrid").addEventListener("click", resetGrid);
+
+    /** @type {HTMLInputElement} */
+    const elWrapGrid = document.getElementById("wrapGrid");
+    wrapGrid = elWrapGrid.checked;
+    elWrapGrid.addEventListener("change", () => {
+        wrapGrid = elWrapGrid.checked;
+    });
+
+    resetGrid();
+    recalcGridSize(canvas);
+
+    const setCanvasCell = (cX, cY, cell) => {
+        const cellX = Math.floor((cX - GRID_ORIGIN.x) / CELL_SIZE);
+        const cellY = Math.floor((cY - GRID_ORIGIN.y) / CELL_SIZE);
+        setCell(cellX, cellY, cell);
+    }
+
+    canvas.element.addEventListener("mousedown", ev => {
+        setCanvasCell(ev.x, ev.y, ev.ctrlKey ? DEAD_CELL : LIVE_CELL);
+    });
+
+    canvas.element.addEventListener("mousemove", ev => {
+        if (ev.buttons === 1) {
+            setCanvasCell(ev.x, ev.y, ev.ctrlKey ? DEAD_CELL : LIVE_CELL);
+        }
     });
 });
